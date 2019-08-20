45122af6242ffffbd9d27a5ff5a47084dbda2203
Rakesh <rakesh@codetoart.com>
2019-08-20 15:05:52 +0530

    Elastic Search connection and Dropdown selection complete

--- app.py
+++ app.py
@@ -1,18 +1,67 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+
+from elasticsearch import Elasticsearch
+es = Elasticsearch('http://localhost:9200')
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/dd")
 def dd():
     return render_template("dependent_dropdown.html")
+
+
+@app.route("/get_country", methods=['GET'])
+def get_country():
+    country_data = es.search(index='my_country_index_3', body={'query': {"match": {"_type": "country"}}},
+              filter_path=['hits.hits._id', 'hits.hits._source'])
+    if 'hits' in country_data and 'hits' in country_data['hits']:
+        return jsonify({"country": [data["_source"]["name"] for data in country_data['hits']['hits']]}), 200
+    else:
+        return jsonify({"error": True, "message": "No data present for countries"}), 200
+
+
+@app.route("/get_state", methods=['GET'])
+def get_state():
+    country = request.args.get("country", type=str)
+    if not country:
+        return jsonify({"message": "Please set the country field"}), 400
+    state_data = es.search(
+        index='my_country_index_3',
+        body={'query': {"bool": {"must": [{"match": {"_type": "state"}}, {"match": {"country": country}}]}}},
+        filter_path=['hits.hits._id', 'hits.hits._source']
+    )
+    if 'hits' in state_data and 'hits' in state_data['hits']:
+        return jsonify({"state": [data["_source"]["name"] for data in state_data['hits']['hits']]}), 200
+    else:
+        return jsonify({"error": True, "message": "No state data present for country - " + country}), 200
+
+
+@app.route("/get_city", methods=['GET'])
+def get_city():
+    state = request.args.get("state", type=str)
+    if not state:
+        return jsonify({"message": "Please set the state field"}), 400
+    city_data = es.search(
+        index='my_country_index_3',
+        body={'query': {"bool": {"must": [{"match": {"_type": "city"}}, {"match": {"state": state}}]}}},
+        filter_path=['hits.hits._id', 'hits.hits._source']
+    )
+    if 'hits' in city_data and 'hits' in city_data['hits']:
+        return jsonify({"city": [data["_source"]["name"] for data in city_data['hits']['hits']]}), 200
+    else:
+        return jsonify({"error": True, "message": "No city data present for state - " + state}), 200
+
 
 @app.route("/crud")
 def crud():
     return render_template("crud.html")
 
+
 if __name__ == "__main__":
-    app.run(port=5000, debug=True, host="0.0.0.0")
+    app.run(port=5005, debug=True, host="0.0.0.0")
--- static/js/dependent_dropdown.js
+++ static/js/dependent_dropdown.js
@@ -1,3 +1,127 @@
 $(document).ready(function() {
-	console.log("Document is ready")
+
+//    Get Country Data
+    var country, state, city
+    $.ajax({
+        data : {},
+        type : 'GET',
+        url : '/get_country',
+        beforeSend: function() {
+            reset_country_dd()
+            set_selected_city_label()
+        },
+        success: function(data) {
+            if (data.error) {
+                console.log("Got error")
+                if (data.message != undefined) {
+                    set_selected_city_label(data.message, 'red')
+                }
+            }
+            else {
+                if (data.country != undefined) {
+                    $.each(data.country, function(index, value){
+                        $('#country_dd').append(new Option(value, value));
+                    })
+                }
+                set_selected_city_label('Select Country')
+            }
+        },
+        error: function() {
+            console.log("Got error for request")
+        }
+    });
+
+	$("#country_dd").off("change");
+    $("#country_dd").on("change", function(){
+        country = this.value
+        $.ajax({
+            data : {
+                country: country,
+            },
+            type : 'GET',
+            url : '/get_state',
+            beforeSend: function() {
+                reset_state_dd()
+                set_selected_city_label()
+            },
+            success: function(data) {
+                if (data.error) {
+                    console.log("Got error")
+                    if (data.message != undefined) {
+                        set_selected_city_label(data.message, 'red')
+                    }
+                }
+                else {
+                    if (data.state != undefined) {
+                        $.each(data.state, function(index, value){
+                            $('#state_dd').append(new Option(value, value));
+                        })
+                    }
+                    set_selected_city_label('Select State')
+                }
+            },
+            error: function() {
+                console.log("Got error for request")
+            }
+        });
+    });
+
+	$("#state_dd").off("change");
+    $("#state_dd").on("change", function(){
+        state = this.value
+        $.ajax({
+            data : {
+                state: state,
+            },
+            type : 'GET',
+            url : '/get_city',
+            beforeSend: function() {
+                reset_city_dd()
+                set_selected_city_label()
+            },
+            success: function(data) {
+                if (data.error) {
+                    console.log("Got error")
+                    if (data.message != undefined) {
+                        set_selected_city_label(data.message, 'red')
+                    }
+                }
+                else {
+                    if (data.city != undefined) {
+                        $.each(data.city, function(index, value){
+                            $('#city_dd').append(new Option(value, value));
+                        })
+                    }
+                    set_selected_city_label('Select City')
+                }
+            },
+            error: function() {
+                console.log("Got error for request")
+            }
+        });
+    });
+
+	$("#city_dd").off("change");
+    $("#city_dd").on("change", function(){
+        console.log(this.value);
+        set_selected_city_label('You have selected City: ' + this.value, 'green')
+    });
+
+    function reset_country_dd(){
+        $("#country_dd").html('<option value="" selected="selected">Select Country</option>')
+        reset_state_dd()
+    }
+
+    function reset_state_dd(){
+        $("#state_dd").html('<option value="" selected="selected">Select State</option>')
+        reset_city_dd()
+    }
+
+    function reset_city_dd(){
+        $("#city_dd").html('<option value="" selected="selected">Select City</option>')
+    }
+
+    function set_selected_city_label(data='', color='black'){
+        $("#selected_city").html(data).css('color', color)
+    }
 });
